304f2877761718eb5613f3875b72402fefea5ce6
sarvesh <dubeysarvesh5525@gmail.com>
2021-03-28 17:28:10 +0530

    session

--- src/components/session.js
+++ src/components/session.js
@@ -1,9 +1,27 @@
-import { Button} from "@chakra-ui/react"
+import { Button } from "@chakra-ui/react"
+import './session.css'
 
 export default function Session() {
+
+    function getRandomInt() {
+        let max = JSON.parse( localStorage.getItem( 'frontface' ) ).length
+        let rand = Math.floor( Math.random() * Math.floor( max ) );
+        let arr = JSON.parse( localStorage.getItem( 'frontface' ) )
+        return arr[rand].short_story
+    }
+
+    function reload() {
+        window.location.reload();
+    }
+
     return (
-        <div>
-            <Button ml={4} mt={4}>Start Session</Button>
+        <div className="grid-sys">
+            <div className="test-card">
+                <h1 className = "qtext">{getRandomInt()}</h1>
+            </div>
+            <div className="yes-no">
+                <Button onClick = {reload} m = {4}>Generate Random</Button>
+            </div>
         </div>
     )
 }
